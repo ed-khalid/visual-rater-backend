97d53030d18ebaa3eb86b5259ea555f9c4a3cc2f
Ed Khalid <ed.khalid1@gmail.com>
2023-07-22 15:46:09 -0400

    fix 0.5 comparison step bug

--- db/05.get_comparison_songs.sql
+++ db/05.get_comparison_songs.sql
@@ -1,11 +1,11 @@
 DROP FUNCTION IF EXISTS get_other_artists_comparison_songs(songId uuid, excludedArtistId uuid);
 CREATE OR REPLACE FUNCTION get_other_artists_comparison_songs(songId uuid, excludedArtistId uuid)
-RETURNS TABLE(score double precision, song_name varchar(100), artist_name varchar(100), album_name varchar(100), album_thumbnail varchar(100), album_dominant_color varchar(15))
+RETURNS TABLE(score double precision, song_id uuid, song_name varchar(100), artist_name varchar(100), album_name varchar(100), album_thumbnail varchar(100), album_dominant_color varchar(15))
 LANGUAGE plpgsql
 AS $$
     BEGIN
-    RETURN QUERY SELECT s.score, s.name, a.name, alb.name, alb.thumbnail, alb.dominant_color from generate_series(0,5,0.5) indexScore
-    CROSS JOIN LATERAL  (SELECT s.score, s.name, s.artist_id, s.album_id from Song s where s.score = indexScore and s.id != songId LIMIT 1) s
+    RETURN QUERY SELECT s.score, s.id, s.name, a.name, alb.name, alb.thumbnail, alb.dominant_color from generate_series(0,5,0.05) indexScore
+    CROSS JOIN LATERAL  (SELECT s.score, s.id, s.name, s.artist_id, s.album_id from Song s where s.score = indexScore and s.id != songId LIMIT 1) s
     INNER JOIN artist a on a.id = s.artist_id
     INNER JOIN album alb on alb.id = s.album_id
     where a.id != excludedArtistId
@@ -16,12 +16,12 @@ $$;
 
 DROP FUNCTION IF EXISTS get_artist_comparison_songs(songId uuid, artistId uuid, excludedAlbumId uuid);
 CREATE OR REPLACE FUNCTION get_artist_comparison_songs(songId uuid, artistId uuid, excludedAlbumId uuid)
-  RETURNS TABLE(score double precision, song_name varchar(100), artist_name varchar(100), album_name varchar(100), album_thumbnail varchar(100), album_dominant_color varchar(15))
+  RETURNS TABLE(score double precision, song_id uuid, song_name varchar(100), artist_name varchar(100), album_name varchar(100), album_thumbnail varchar(100), album_dominant_color varchar(15))
   LANGUAGE plpgsql
 AS $begin$
         BEGIN
-RETURN QUERY SELECT s.score, s.name, a.name, alb.name, alb.thumbnail, alb.dominant_color from generate_series(0,5,0.05) indexScore
-                                                                 CROSS JOIN LATERAL  (SELECT s.score, s.name, s.artist_id, s.album_id from Song s where s.score = indexScore and s.id != songId LIMIT 1) s
+RETURN QUERY SELECT s.score, s.id, s.name, a.name, alb.name, alb.thumbnail, alb.dominant_color from generate_series(0,5,0.05) indexScore
+                                                                 CROSS JOIN LATERAL  (SELECT s.score,s.id, s.name, s.artist_id, s.album_id from Song s where s.score = indexScore and s.id != songId LIMIT 1) s
                                                                  INNER JOIN artist a on a.id = s.artist_id
                                                                  INNER JOIN album alb on alb.id = s.album_id
 where a.id = artistId  and alb.id != excludedAlbumId
